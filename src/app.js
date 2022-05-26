9e679c0465e96bca22438723a5cb99dd10b85532
Phan Anh Tuan <tuancdt067@gmail.com>
2022-05-26 09:14:04 +0700

    fix

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = 3001;
 const router = require('./router/index');
 const db = require('./config/db');
 const Staff = require('./models/staff');
