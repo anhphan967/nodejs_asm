8fd9bdf1a99f3951b81b62fa2b2006394fb7f827
Phan Anh Tuan <tuancdt067@gmail.com>
2022-06-04 14:24:45 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const port = 3001;
 const router = require('./router/index');
 const db = require('./config/db');
 const Staff = require('./models/staff');
@@ -10,7 +10,7 @@ const csrf = require('csurf');
 const mongodbStore = require('connect-mongodb-session')(session);
 const multer = require('multer');
 const flash = require('connect-flash');
-const path= require('path')
+const path = require('path')
 
 const MONGODB_URI = 'mongodb+srv://asm2:batho123@cluster0.jvmuo.mongodb.net/';
 ;
@@ -33,46 +33,46 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'public/images');
-    },
-    filename: (req, file, cb) => {    
-      cb(null,new Date().toISOString().replace(/:/g,"-")+ '-' + file.originalname);
-    }
-  });
-  
-  const fileFilter = (req, file, cb) => {
-    if (
-      file.mimetype === 'image/png' ||
-      file.mimetype === 'image/jpg' ||
-      file.mimetype === 'image/jpeg'
-    ) {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
-  };
+  destination: (req, file, cb) => {
+    cb(null, 'public/images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString().replace(/:/g, "-") + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 // save session
 const store = new mongodbStore({
-    uri: MONGODB_URI,
-    collection: 'sessions',
+  uri: MONGODB_URI,
+  collection: 'sessions',
 });
 app.use(
-    session({
-        secret: 'my secret',
-        resave: false,
-        saveUninitialized: false,
-        store,
-    })
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store,
+  })
 );
 // Set static: public:
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images',express.static(path.join(__dirname,'public' ,'images')));  
+app.use('/images', express.static(path.join(__dirname, 'public', 'images')));
 app.use(
-    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
-  );
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 
 // Token
 const csrfProtection = csrf();
@@ -80,40 +80,40 @@ app.use(csrfProtection);
 
 // set token, authenticated
 app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 // Add staff in request
-app.use((req, res, next) => {  
-  if (!req.session.staff) {        
-      res.locals.account = false;  
+app.use((req, res, next) => {
+  if (!req.session.staff) {
+    res.locals.account = false;
+    return next();
+  }
+  Staff.findById(req.session.staff._id)
+    .then((staff) => {
+      if (!staff) {
         return next();
-    }
-    Staff.findById(req.session.staff._id)
-        .then((staff) => {
-            if (!staff) {
-                return next();
-            }
-            req.staff = staff;
-            res.locals.loggedIn = true;
-            res.locals.auth = staff;  
-            if(staff.account=='admin'){
-              res.locals.account='admin'
-              return next()
-            }  
-            res.locals.account='staff'            
-            next();
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+      }
+      req.staff = staff;
+      res.locals.loggedIn = true;
+      res.locals.auth = staff;
+      if (staff.account == 'admin') {
+        res.locals.account = 'admin'
+        return next()
+      }
+      res.locals.account = 'staff'
+      next();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 // Init router
 router(app);
 
-app.listen(port, () => {
-    console.log(`App running at http://localhost:${port}`);
+app.listen(process.env.PORT || port, () => {
+  console.log(`App running at http://localhost:${port}`);
 });
